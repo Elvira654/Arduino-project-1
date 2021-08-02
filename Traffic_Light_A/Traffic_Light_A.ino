ccab120c4df5b23b516ccb005e038bd33f641ea8
Elvira <57684716+Elvira654@users.noreply.github.com>
2021-08-02 14:09:17 +0200

    Add files via upload

---
+++ Traffic_Light_A/Traffic_Light_A.ino
@@ -0,0 +1,36 @@
+int led1= 10 ;
+int led2= 9 ;
+int led3= 8 ;
+
+void setup() {
+pinMode(led1, OUTPUT) ;
+pinMode(led2, OUTPUT) ;
+pinMode(led3, OUTPUT) ;
+
+
+}
+
+void loop() {
+   digitalWrite(led1, HIGH) ;
+  delay(8000);
+   digitalWrite(led2, HIGH) ;
+  delay(1000);
+   digitalWrite(led1, LOW) ;
+   digitalWrite(led2, LOW) ;
+   digitalWrite(led3, HIGH) ;
+  delay(8000);
+   digitalWrite(led3, LOW) ;
+  delay(1000);
+   digitalWrite(led3, HIGH) ;
+     delay(1000);
+   digitalWrite(led3, LOW) ;
+   digitalWrite(led2, HIGH) ;
+  delay(2000);
+ digitalWrite(led2, LOW) ;
+                                            
+  
+  
+  
+  
+
+}
